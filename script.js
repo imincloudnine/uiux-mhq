baf6d15a57e20a5e98b9eef0ef80fa50699d7dfd
imincloudnine <139517132+imincloudnine@users.noreply.github.com>
2024-11-24 23:35:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
 // Variables to keep track of current question and user responses
 const questions = [
     "Little interest or pleasure in doing things",
